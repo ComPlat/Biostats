57d2c26b198012b9db68d00e0ab71c8c47c2e40f
Konrad1991 <konrad_kraemer@yahoo.de>
2024-03-20 15:51:35 +0100

    created static js code

--- deploy.R
+++ deploy.R
@@ -2,7 +2,6 @@ setwd("/home/konrad/Documents/GitHub/shinychem")
 shinylive::export("./BiostatsGithubPage", "docs", verbose = TRUE)
 httpuv::runStaticServer("docs")
 
-
 setwd("/home/konrad/Documents/GitHub/shinychem/BiostatsGithubPage")
 source("/home/konrad/Documents/GitHub/shinychem/BiostatsGithubPage/app.R")
 shinyApp(ui, server)
