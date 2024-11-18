be3161280afdc980571c9fd8bf1ebd66fd06eb19
Konrad1991 <konrad_kraemer@yahoo.de>
2024-11-18 15:58:19 +0100

    update of the docu new part for ecdf

--- bs/.development/run_app.R
+++
@@ -1,9 +0,0 @@
-# Run in browser
-
-# setwd("/home/konrad/Documents/Biostats")
-# shinylive::export("./App", "docs", verbose = TRUE)
-# httpuv::runStaticServer("docs")
-
-Sys.setenv(RUN_MODE = "BROWSER")
-library(bs)
-run_app()
