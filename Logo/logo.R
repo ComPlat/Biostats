6256abbed7667ab915b3490ea4456e53625c08d4
Konrad1991 <konrad_kraemer@yahoo.de>
2025-02-26 08:21:34 +0100

    added an alternative logo

--- Logo/logo.R
+++ Logo/logo.R
@@ -1,5 +1,8 @@
 library(hexSticker)
 library(ggplot2)
+
+# Idea: Penguins
+# ==========================================================================
 library(palmerpenguins)
 library(rphylopic)
 df <- palmerpenguins::penguins |> as.data.frame()
@@ -28,13 +31,13 @@ p <- ggplot() +
     data = data[data$Species == "Adelie", ],
     img = penguin1,
     aes(x = Species, y = Mean_Bill_Length),
-    height = penguin_size, show.legend = FALSE, colour = "orange"
+    height = penguin_size, show.legend = FALSE, colour = "black"
   ) +
   geom_phylopic(
     data = data[data$Species == "Gentoo", ],
     img = penguin2,
     aes(x = Species, y = Mean_Bill_Length),
-    height = penguin_size, show.legend = FALSE, colour = "darkblue"
+    height = penguin_size, show.legend = FALSE, colour = "black"
   ) +
   geom_phylopic(
     data = data[data$Species == "Chinstrap", ],
@@ -42,7 +45,7 @@ p <- ggplot() +
     aes(x = Species, y = Mean_Bill_Length),
     height = penguin_size, show.legend = FALSE
   ) +
-  labs(y = "Mean Beak length [mm]", x = "Species") +
+  labs(y = "Mean Beak length [mm]", x = NULL) +
   theme_minimal() +
   theme(
     legend.position = "inside", legend.position.inside = c(0.9, 0.9),
@@ -53,18 +56,70 @@ p <- ggplot() +
   ) +
   ylim(y_min, y_max)
 
-sticker <- sticker(
-  subplot = p,
-  package = "Biostats",
-  p_color = "black",
-  s_x = 0.95, s_y = 1,
-  s_width = 1.4, s_height = 1.4,
-  p_size = 12,
-  p_y = 1.5,
-  h_fill = "#76acb0",
-  h_color = "black",
-  filename = "BiostatsLogo.svg"
-)
+create_sticker <- function(p) {
+  sticker <- sticker(
+    subplot = p,
+    package = "Biostats",
+    p_color = "black",
+    s_x = 0.95, s_y = 1,
+    s_width = 1.4, s_height = 1.4,
+    p_size = 16,
+    p_y = 1.5,
+    h_fill = "#76acb0",
+    h_color = "black",
+    filename = "BiostatsLogo.svg"
+  )
+  dev.off() # Dont know what is printed here but it is anying
+  ggsave("BiostatsLogo.svg",
+    sticker,
+    width = 4, height = 4,
+    dpi = 600, bg = "white"
+  )
+}
+create_sticker(p)
+system("open BiostatsLogo.svg")
 
-print(sticker)
-ggsave("BiostatsLogo.svg", sticker, width = 4, height = 4, bg = "white")
+# Idea: Overlapping density plots
+# ==========================================================================
+set.seed(123)
+df <- data.frame(y1 = rnorm(1000), y2 = rnorm(1000, mean = 2))
+
+p <- ggplot() +
+  geom_density(
+    data = df,
+    aes(x = y2), # Plot the white curve first
+    fill = "white",
+    color = "black",
+    alpha = 0.9, linewidth = 1.2
+  ) +
+  geom_density(
+    data = df,
+    aes(x = y1), # Then plot the black curve on top
+    fill = "black",
+    alpha = 0.6,
+    color = "black",
+    linewidth = 1.2
+  ) +
+  theme_void()
+create_sticker <- function(p) {
+  sticker <- sticker(
+    subplot = p,
+    package = "Biostats",
+    p_color = "black",
+    s_x = 1, s_y = 0.9,
+    s_width = 1.4, s_height = 0.9,
+    p_size = 18,
+    p_y = 1.5, p_x = 1.05,
+    h_fill = "#76acb0",
+    h_color = "black",
+    filename = "BiostatsLogo2.svg"
+  )
+  dev.off() # Dont know what is printed here but it is anoying
+  ggsave("BiostatsLogo2.svg",
+    sticker,
+    width = 4, height = 4,
+    dpi = 600, bg = "white"
+  )
+}
+create_sticker(p)
+system("open BiostatsLogo2.svg")
