076a8c5cfa8b218279fd45d7dec55314b7588346
Konrad1991 <konrad_kraemer@yahoo.de>
2025-02-26 07:34:07 +0100

    simplified logo for Biostats

--- Logo/logo.R
+++ Logo/logo.R
@@ -4,9 +4,9 @@ library(palmerpenguins)
 library(rphylopic)
 df <- palmerpenguins::penguins |> as.data.frame()
 
-penguin1 <- pick_phylopic("Pygoscelis", n = 3, view = 3) # click 2
-penguin2 <- pick_phylopic("Pygoscelis", n = 3, view = 3) # click 3
-penguin3 <- pick_phylopic("Pygoscelis", n = 9, view = 9) # click 4
+penguin1 <- rphylopic::get_phylopic("86334821-42ec-4da1-bb9d-53f3d6941c77")
+penguin2 <- rphylopic::get_phylopic("a4112793-e464-4d6f-8bde-8976bcca100a")
+penguin3 <- rphylopic::get_phylopic("9940201e-c445-4ff9-9742-8c3bdac04643")
 pen1 <- rotate_phylopic(img = penguin1, angle = 15)
 pen2 <- rotate_phylopic(img = penguin2, angle = 15)
 pen3 <- rotate_phylopic(img = penguin3, angle = 15)
@@ -42,15 +42,14 @@ p <- ggplot() +
     aes(x = Species, y = Mean_Bill_Length),
     height = penguin_size, show.legend = FALSE
   ) +
-  labs(y = "Mean Bill length [mm]", x = "Species") +
+  labs(y = "Mean Beak length [mm]", x = "Species") +
   theme_minimal() +
   theme(
     legend.position = "inside", legend.position.inside = c(0.9, 0.9),
     axis.title = element_text(size = text_size, face = "bold"),
-    axis.text = element_text(size = text_size, face = "bold"),
     legend.text = element_text(size = text_size, face = "bold"),
     legend.title = element_text(size = text_size, face = "bold"),
-    axis.text.x = element_text(angle = 45, hjust = 1)
+    axis.text = element_blank()
   ) +
   ylim(y_min, y_max)
 
@@ -58,11 +57,14 @@ sticker <- sticker(
   subplot = p,
   package = "Biostats",
   p_color = "black",
-  s_x = 1, s_y = 1,
+  s_x = 0.95, s_y = 1,
   s_width = 1.4, s_height = 1.4,
-  p_size = 14,
+  p_size = 12,
+  p_y = 1.5,
   h_fill = "#76acb0",
   h_color = "black",
   filename = "BiostatsLogo.svg"
 )
+
+print(sticker)
 ggsave("BiostatsLogo.svg", sticker, width = 4, height = 4, bg = "white")
