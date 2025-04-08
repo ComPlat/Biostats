095c89d64286805fb513724585ca724018d36438
Konrad1991 <konrad_kraemer@yahoo.de>
2025-04-08 10:38:43 +0200

    fixed layout of MTT report due to update in quarto

--- MTT/R/create_pdf.R
+++ MTT/R/create_pdf.R
@@ -13,37 +13,60 @@ report_plots <- function(ic50List) {
       next
     }
     p1 <- ic50List[[i]][[2]]
-    a <- ic50List[[i]][[1]] |> t() |> as.data.frame() 
+    a <- ic50List[[i]][[1]]
+    a <- a |> t() |> as.data.frame()
     a <- data.frame(names = row.names(a), Predicition = a)
     a[a$names == "Response_lowestdose_predicted", 1] <- "Response_lowestdose"
     a[a$names == "Response_highestdose_predicted", 1] <- "Response_highestdose"
     problem <- a[a$names == "Problems", 2]
     a <- a[(a$names != "Problems") & (a$names != "name"), ]
+
+    # Generate horizontal line positions between rows
+    line_df <- data.frame(y = seq_len(nrow(a)) - 0.5)
+
     p2 <- ggplot(a, aes(x = 0, y = factor(names), label = Prediction)) +
+      # geom_hline(data = line_df, aes(yintercept = y), color = "grey80", inherit.aes = FALSE) +
       geom_line(size = 0) +
       geom_text(position = position_nudge(x = -1.1), hjust = 0, size = 3) +
       theme_minimal() +
       theme(axis.text.x = element_blank(),
-            axis.ticks.x = element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.grid.minor.x = element_blank(),
-            panel.grid.major.y = element_blank(),
-            panel.grid.minor.y = element_blank(),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.y = element_text(hjust = 0, face = "bold"),
-            axis.line.y = element_line(),
-            plot.caption = element_text(hjust = 1, face = "italic", colour = "darkred", 
-                                        size = 7) ) 
+        axis.ticks.x = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(hjust = 0, face = "bold"),
+        axis.line.y = element_line(),
+        plot.caption = element_text(hjust = 1, face = "italic", colour = "darkred", 
+          size = 7))
+
+
+    # p2 <- ggplot(a, aes(x = 0, y = factor(names), label = Prediction)) +
+    #   geom_line(size = 0) +
+    #   geom_text(position = position_nudge(x = -1.1), hjust = 0, size = 3) +
+    #   theme_minimal() +
+    #   theme(axis.text.x = element_blank(),
+    #         axis.ticks.x = element_blank(),
+    #         panel.grid.major.x = element_blank(),
+    #         panel.grid.minor.x = element_blank(),
+    #         panel.grid.major.y = element_blank(),
+    #         panel.grid.minor.y = element_blank(),
+    #         axis.title.x = element_blank(),
+    #         axis.title.y = element_blank(),
+    #         axis.text.y = element_text(hjust = 0, face = "bold"),
+    #         axis.line.y = element_line(),
+    #         plot.caption = element_text(hjust = 1, face = "italic", colour = "darkred", 
+    #                                     size = 7) ) 
     if(problem != "") {
       p2 <- p2 + labs(caption = paste("Note:", as.character(problem)) )
     }
-      
-    p <- ggdraw() +
-      draw_plot(p2, x = 0, y = 0, width = 0.5, height = 0.5) +
-      draw_plot(p1, x = 0.5, y = 0, width = 0.5, height = 0.5) 
+
+    bar <- ggplot() +
+      geom_rect(aes(xmin = 0, xmax = 1, ymin = 0, ymax = 1),
+        fill = "black") +
+      theme_void()
+
+    p <- plot_grid(p2, p1, bar, rel_heights = c(0.75, 1, 0.0025), ncol = 1)
     print(p)
-    print(p3)
+
   }
 }
 
